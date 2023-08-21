b158a62f8a1a4e464a57b1ebf6183f45996b9665
ajdinl <ajdinl@live.com>
2023-08-21 10:06:04 +0200

    remove async

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
   description: 'A simple application to create and send email templates',
 }
 
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <Provider>
