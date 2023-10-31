e16fd5c9c767716401d8429d6e2ca2eae4ff800c
ajdinl <ajdinl@live.com>
2023-10-31 19:50:23 +0100

    move Header and Sidebar content to components

---
+++ client/app/_components/SideBar.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { BsFillSendFill, BsFillPersonFill, BsInbox } from 'react-icons/bs'
+import { RxExit } from 'react-icons/rx'
+import { MdEmail } from 'react-icons/md'
+export default function SideBar() {
+  return (
+    <div className='app-sidebar'>
+      <div className='app-sidebar__upper'>
+        <Link href='/'>
+          <MdEmail className='app-sidebar__upper__logo'></MdEmail>
+        </Link>
+        <ul>
+          <li>
+            <BsFillPersonFill className='app-sidebar__upper__icon1'></BsFillPersonFill>
+          </li>
+          <li>
+            <BsFillSendFill className='app-sidebar__upper__icon2'></BsFillSendFill>
+          </li>
+          <li>
+            <BsInbox className='app-sidebar__upper__icon3'></BsInbox>
+          </li>
+        </ul>
+      </div>
+      <div className='app-sidebar__lower'>
+        <ul>
+          <li>
+            <RxExit className='app-sidebar__upper__icon4'></RxExit>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
