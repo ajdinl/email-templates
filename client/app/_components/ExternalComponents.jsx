9f87c3706dd47d9a92a04daf0a7686fa33abacf7
ajdinl <ajdinl@live.com>
2023-10-31 20:04:44 +0100

    move external components to ExternalComponents component and export

---
+++ client/app/_components/ExternalComponents.jsx
@@ -0,0 +1,30 @@
+import { BiUpArrowAlt, BiDownArrowAlt } from 'react-icons/bi'
+import { signOut, useSession } from 'next-auth/react'
+import { ImSpinner9 } from 'react-icons/im'
+import { RxExit } from 'react-icons/rx'
+import { MdEmail } from 'react-icons/md'
+import { Menu, MenuItem } from '@szhsin/react-menu'
+import Moment from 'react-moment'
+import {
+  BsFillSendFill,
+  BsFillPersonFill,
+  BsInbox,
+  BsSearch,
+} from 'react-icons/bs'
+
+export {
+  BiUpArrowAlt,
+  BiDownArrowAlt,
+  signOut,
+  useSession,
+  ImSpinner9,
+  BsSearch,
+  BsFillSendFill,
+  BsFillPersonFill,
+  BsInbox,
+  MdEmail,
+  RxExit,
+  Menu,
+  MenuItem,
+  Moment,
+}
